8f0f18406b575dd2c6183f3c826e78b2b3f05359
mrshahriar <toshahriarshakil@gmail.com>
2020-08-26 16:31:54 +0600

    Updates

---
+++ precache-manifest.7124f44335b7d1d7d1f051c24355459c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c96d195c3f46f7b51c3bacd38d93d885",
+    "url": "/react-chat-ui/index.html"
+  },
+  {
+    "revision": "987b8e2b1a06e0497d3d",
+    "url": "/react-chat-ui/static/css/2.fab5e3fc.chunk.css"
+  },
+  {
+    "revision": "ad11c66825e6ac75c3d8",
+    "url": "/react-chat-ui/static/css/main.b6d5ddf0.chunk.css"
+  },
+  {
+    "revision": "987b8e2b1a06e0497d3d",
+    "url": "/react-chat-ui/static/js/2.eac300ef.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/react-chat-ui/static/js/2.eac300ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad11c66825e6ac75c3d8",
+    "url": "/react-chat-ui/static/js/main.56dc1749.chunk.js"
+  },
+  {
+    "revision": "bbab760d28e792418bd3",
+    "url": "/react-chat-ui/static/js/runtime-main.f5718efd.js"
+  },
+  {
+    "revision": "44d3b27b4f4ad78d3b6eeccae8c01ef4",
+    "url": "/react-chat-ui/static/media/search.44d3b27b.svg"
+  }
+]);
